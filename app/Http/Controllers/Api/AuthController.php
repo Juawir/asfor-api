e57fd6af2fb8cdd9d6b751eb6368773d4e686d26
Steven Gerrard <ytbreven@gmail.com>
2026-05-05 16:00:29 +0700

    perbaikan api v3

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -16,17 +16,17 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'email' => 'required',
+            'identity' => 'required',
             'password' => 'required',
         ]);
 
-        $user = User::where('email', $request->email)
-                    ->orWhere('name', $request->email)
+        $user = User::where('email', $request->identity)
+                    ->orWhere('name', $request->identity)
                     ->first();
 
         if (! $user || ! Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
-                'email' => ['Kredensial yang diberikan salah.'],
+                'identity' => ['Kredensial yang diberikan salah.'],
             ]);
         }
 
